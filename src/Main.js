06d388c98a09b15e45acf279b77690697c08d384
TheChaoticWeb <96139927+TheChaoticWeb@users.noreply.github.com>
2022-03-28 11:05:24 -0700

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ const{Terminal}=require('./Terminal');
 class Main {
   static main(args) {
     new Server().listen(function(){
-      Terminal.info("Server is up and running.")
+      Terminal.print("Server is up and running.")
     })
     return 0;
   }
