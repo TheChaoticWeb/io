d7900b0fc0cf16545778c9028a96ae9271b344b2
TheChaoticWeb <96139927+TheChaoticWeb@users.noreply.github.com>
2022-03-28 11:18:02 -0700

    Update Server.js

--- src/Server.js
+++ src/Server.js
@@ -1,4 +1,33 @@
+const express=require('express');
+const{createServer}=require('http');
+const socket$io=require('socket.io');
+const{Terminal}=require('./Terminal');
+
 class Server {
-  
+  constructor() {
+    this.init();
+    
+    this.socketRoute();
+    this.httpRoute();
+  }
+  init() {
+    this.app = express();
+    this.http = createServer(this.app);
+    this.io = socket$io(this.http);
+  }
+  socketRoute() {
+    this.io.on('connection', function(socket) {
+      socket.emit('connection');
+      socket.on('core:ping', function() {
+        Terminal.print("Got a ping from "+socket.id);
+      });
+    });
+  }
+  httpRoute() {
+    
+  }
+  listen(cb) {
+    this.http.listen(8888, cb);
+  }
 }
 module.exports={Server}
