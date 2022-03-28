e664ec387c91c132f33848631e5253223b9c6d53
TheChaoticWeb <96139927+TheChaoticWeb@users.noreply.github.com>
2022-03-28 16:05:06 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,12 @@
 addEventListener("fetch", function(event) {
-  event.respondWith(new Response("Hello, World!"));
+  var url = new URL(event.request.url);
+  if(url.pathname.startsWith('/socket.io/')) {
+    fetch(event.request).then(function(response) {
+      response.headers.set("Access-Aontrol-Allow-Origin", "*");
+      event.respondWith(response);
+    }, function(err) {
+      event.respondWith(new Response(err,{status:500}));
+    });
+  } else {
+  }
 });
