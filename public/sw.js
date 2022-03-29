d9df6659883aaac0d9abf8325d63843a93f5efee
TheChaoticWeb <96139927+TheChaoticWeb@users.noreply.github.com>
2022-03-29 11:16:22 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,19 @@
 addEventListener("fetch", function(event) {
   var url = new URL(event.request.url);
   if(url.pathname.startsWith('/socket.io/') || url.pathname == "/__devtools_wrapper.html") {
-    fetch(event.request).then(async function(response) {
+    fetch(event.request).then(function(response) {
       var res = new Response(response);
       res.headers.set("Access-Control-Allow-Origin", "*");
-      event.respondWith(res.status.toString().startsWith("4") ? Response.redirect("/FatalError.sh?~="+encodeURIComponent(await res.text())) : res);
+      event.respondWith(new Promise(function(resolve, reject) {
+        if(res.status.toString().startsWith("4")) {
+          res.text().then(function(text) {
+            resolve(Response.redirect("/FatalError.sh?~="+encodeURIComponent(text)));
+          });
+        }
+        else {
+          resolve(res);
+        }
+      });
     }, function(err) {
       event.respondWith(Response.redirect("/FatalError.sh?~="+encodeURIComponent(err)));
     });
